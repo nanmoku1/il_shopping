32092fdb583651a93d5ffb3bec4374a9f9c0f8cc
bessho-s <maeshing67@gmail.com>
2020-04-22 12:12:07 +0900

    不適切なコントローラークラス名を修正

--- application/routes/web.php
+++ application/routes/web.php
@@ -29,13 +29,13 @@ Route::prefix('admin')->namespace('Admin')->as('admin.')->group(function () {
     Route::middleware('auth:admin')->group(function () {
         Route::post('logout', 'LoginController@logout')->name('logout');
         Route::get('home', 'HomeController@index')->name('home');
-        Route::get('admin_users', 'AdminUsersController@index')->name('admin_users_list');
-        Route::get('admin_users/create', 'AdminUsersController@create_page')->name('admin_users_create_page');
-        Route::get('admin_users/{id}/edit', 'AdminUsersController@edit_page')->name('admin_users_edit_page');
-        Route::get('admin_users/{id}', 'AdminUsersController@detail')->name('admin_users_detail');
-        Route::post('admin_users', 'AdminUsersController@create')->name('admin_users_create');
-        Route::delete('admin_users/{id}', 'AdminUsersController@delete')->name('admin_users_delete');
-        Route::put('admin_users/edit/{id}', 'AdminUsersController@edit')->name('admin_users_edit');;
+        Route::get('admin_users', 'AdminUserController@index')->name('admin_users_list');
+        Route::get('admin_users/create', 'AdminUserController@create_page')->name('admin_users_create_page');
+        Route::get('admin_users/{id}/edit', 'AdminUserController@edit_page')->name('admin_users_edit_page');
+        Route::get('admin_users/{id}', 'AdminUserController@detail')->name('admin_users_detail');
+        Route::post('admin_users', 'AdminUserController@create')->name('admin_users_create');
+        Route::delete('admin_users/{id}', 'AdminUserController@delete')->name('admin_users_delete');
+        Route::put('admin_users/edit/{id}', 'AdminUserController@edit')->name('admin_users_edit');;
     });
 });
 
