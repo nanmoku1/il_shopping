526a8c097361f59e63e70a30cb6f0af5da6a496c
bessho-s <maeshing67@gmail.com>
2020-04-29 18:08:10 +0900

    管理者管理コントローラーソース内の不要なuseを削除

--- application/app/Http/Controllers/Admin/AdminUserController.php
+++ application/app/Http/Controllers/Admin/AdminUserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\Admin\AdminUsers\AdminUserIndexRequest;
 use App\Http\Requests\Admin\AdminUsers\AdminUserCreateRequest;
 use App\Http\Requests\Admin\AdminUsers\AdminUserEditRequest;
