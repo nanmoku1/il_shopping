a784b5c866950ba388819532caa2a82cd9cf7eeb
bessho-s <maeshing67@gmail.com>
2020-05-20 23:51:05 +0900

    管理画面共通 ・管理者テーブルへのオーナーフラグカラム追加のマイグレーションクラス名に誤りがあった為修正

--- application/database/migrations/2020_04_16_021613_add_is_owner_to_admin_users_table.php
+++ application/database/migrations/2020_04_16_021613_add_is_owner_to_admin_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateAdminUsersTable extends Migration
+class AddIsOwnerToAdminUsersTable extends Migration
 {
     /**
      * Run the migrations.
