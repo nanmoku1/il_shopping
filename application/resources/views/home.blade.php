97da6c2fb7df4bf15ffc32146b48b659ab5e5ec4
“bessho-s” <maeshing67@gmail.com>
2020-06-15 14:23:36 +0900

    ホーム画面 ・ほしい物リストを表示する処理を追加 ・商品リスト表示のうち、商品表示カード部分をコンポーネント化

--- application/resources/views/home.blade.php
+++ application/resources/views/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    @auth('user')
+        <div class="row">
+            <div class="col-md">
+                <h3 class="border-bottom mb-3">ほしいものリスト</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            @foreach(auth("user")->user()->wishProducts as $product)
+                @include('products.components.product_card', ['product' => $product])
+            @endforeach
+        </div>
+    @endauth
 @endsection
--- application/resources/views/products/index.blade.php
+++ application/resources/views/products/index.blade.php
@@ -19,26 +19,7 @@
 
     <div class="row pt-2">
         @foreach($products as $product)
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <a href="{{ route("products.show", $product->id) }}" target="_blank">
-                        @if(filled($product->image_path))
-                            <img class="card-img-top bd-placeholder-img" src="{{ \Storage::url($product->image_path) }}">
-                        @endif
-                    </a>
-                    <div class="card-body">
-                        <a href="{{ route("products.show", $product->id) }}" target="_blank">
-                            <h5 class="card-title">{{ $product->name }}</h5>
-                        </a>
-                        <p class="card-text">¥{{ number_format($product->price) }}</p>
-                        @auth("user")
-                            <a class="toggle_wish" data-product-id="{{ $product->id }}" data-wished="{{ count($product->wishedUsers) > 0 ? "true" : "false" }}">
-                                <i class="fa-heart {{ count($product->wishedUsers) > 0 ? "fas" : "far" }}"></i>
-                            </a>
-                        @endauth
-                    </div>
-                </div>
-            </div>
+            @include('products.components.product_card', ['product' => $product])
         @endforeach
     </div>
 
